f9ceb023758a4a2ebb8085e7468f02e52c602805
Jean Nyilimbibi <jnyilimb@redhat.com>
2024-06-21 10:34:31 +0200

    Minor update

--- Monitoring/Grafana/setup-grafana.sh
+++ Monitoring/Grafana/setup-grafana.sh
@@ -3,5 +3,12 @@
 # Deploy grafana
 oc apply -f ./manifests/grafana.yaml
 
-# Deploy the Prometheus datasource
+# Deploy the Prometheus Gradana Datasource
+# /!\ Make sure the access token is CORRECT
+# Get the token secret from the service account: oc describe sa grafana-oauth-sa
+# Get the token from the secret. E.g., oc extract secret/grafana-oauth-sa-token-28tc2 --to=-
 oc apply -f ./manifests/prometheus_grafanadatasource.yaml
+
+# Deploy the Camel Grafana Dashaboards
+oc apply -f ./manifests/camel-route-metrics_grafanadashboard.yaml
+oc apply -f ./manifests/camel-pod-metrics_grafanadashboard.yaml
